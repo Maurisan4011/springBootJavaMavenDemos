86ed6d685674e874ce3512207606579e72b0a573
Mauricio <maurisan4011@gmail.com>
2020-02-13 02:08:58 -0300

    Servicios REST

---
+++ DemoWeb/src/main/java/com/maudev/controller/DemoController.java
@@ -0,0 +1,37 @@
+package com.maudev.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.maudev.model.Persona;
+import com.maudev.repo.IPersonaRepo;
+
+@Controller
+public class DemoController {
+
+	@Autowired
+	private IPersonaRepo repo;
+	
+	@GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        //logica 
+		
+		Persona m = new Persona();
+		m.setIdPersona(4);
+		m.setNombre("MAuridev");
+		repo.save(m);
+		
+		model.addAttribute("name", name);
+        return "greeting";
+    }
+	
+	@GetMapping("/listar")
+    public String greeting(Model model) {
+        //logica 							
+		model.addAttribute("personas", repo.findAll());
+        return "greeting";
+    }
+}
---
+++ DemoWeb/src/test/java/com/maudev/DemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maudev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
