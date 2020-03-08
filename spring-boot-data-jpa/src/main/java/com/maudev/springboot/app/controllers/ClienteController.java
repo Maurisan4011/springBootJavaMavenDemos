b8e88e4d033ef8b2aa803bb6a4095c83bddd00bb
Mauricio <maurisan4011@gmail.com>
2020-03-07 22:15:20 -0300

    Agregando imagenes a  cada Cliente

--- spring-boot-data-jpa/src/main/java/com/maudev/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/maudev/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,9 @@
 package com.maudev.springboot.app.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -13,12 +17,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.maudev.springboot.app.models.entity.Cliente;
@@ -32,13 +38,25 @@ public class ClienteController {
 	@Autowired
 	private IClienteService clienteService;
 
+	@GetMapping(value = "/ver/{id}")
+	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+
+		Cliente cliente = clienteService.findOne(id);
+		if (cliente == null) {
+			flash.addAttribute("error", "El CLiente no Existe en la base de datos");
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Detalle cliente: " + cliente.getNombre());
+		return "ver";
+	}
+
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
 		Pageable pageRequest = PageRequest.of(page, 5);
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);
-		
-		
-		PageRender<Cliente>pageRender = new PageRender<>("/listar", clientes);
+
+		PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clientes);
 		model.addAttribute("page", pageRender);
@@ -76,11 +94,27 @@ public class ClienteController {
 	}
 
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash,
-			SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
+			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
 		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de Cliente");
 			return "form";
+		}
+		if (!foto.isEmpty()) {
+			Path directorioRecursos = Paths.get("src//main//resources//static/uploads");
+			String rootPath = directorioRecursos.toFile().getAbsolutePath();
+			try {
+				byte[] bytes = foto.getBytes();
+				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				flash.addFlashAttribute("info", "SE Subio correctamente '" + foto.getOriginalFilename() + "'");
+
+				cliente.setFoto(foto.getOriginalFilename());
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		String mensajeFlash = (cliente.getId() != null) ? "Cliente Editado con Exito" : "Cliente Creado con Exito!!";
--- spring-boot-data-jpa/src/main/java/com/maudev/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/maudev/springboot/app/models/entity/Cliente.java
@@ -48,6 +48,8 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 
+	private String foto;
+
 //	@PrePersist
 //	public void prePersist() {
 //		createAt = new Date();
@@ -87,6 +89,14 @@ public class Cliente implements Serializable {
 
 	public Date getCreateAt() {
 		return createAt;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 
 	public void setCreateAt(Date createAt) {
